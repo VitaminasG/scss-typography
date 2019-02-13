5372dc452d908a71045b9955e928a5e7356936a7
Gediminas Palsys <vitaminas.g@gmail.com>
2019-02-13 19:40:18 +0000

    Added 2 responsive files to app.scss
    Signed-off-by: Gediminas Palsys <vitaminas.g@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,54 @@
 <template>
-  <div class="flex-center h-100">
-    <h3>Heading with h3 - tag</h3>
-      <p class="w-50 text-center">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolorum eaque eveniet harum hic id itaque
-        molestiae natus nobis odit, officiis optio quasi quibusdam reiciendis repudiandae sequi tenetur ullam, velit?
+  <div class="flex-center h-100 w-100">
+    <div class="px-2 flex-center h-75 w-75">
+      <div class="flex-center h-25">
+        <img class="h-100" src="./assets/sass.png" alt="SASS">
+      </div>
+      <!-- H1 Tag -->
+      <h1 class="flex-block">
+        <span class="text-alert">&lt;h1&gt;</span>
+        <span class="pl-1">
+          When one handles hypnosis and intuition, one is able to believe anger.
+        </span>
+        <span class="text-alert">&lt;h1&gt;</span>
+      </h1>
+      <!-- H2 Tag -->
+      <h2 class="flex-block">
+        <span class="text-alert">&lt;H2&gt;</span>
+        <span class="pl-1">
+            The scholar believes satori which is not special.
+        </span>
+        <span class="text-alert">&lt;H2&gt;</span>
+      </h2>
+      <!-- H3 Tag -->
+      <h3 class="flex-block">
+        <span class="text-alert">&lt;H3&gt;</span>
+        <span class="pl-1">
+            One united peace i give you: need each other.
+        </span>
+        <span class="text-alert">&lt;H3&gt;</span>
+      </h3>
+      <!-- H4 Tag -->
+      <h4 class="flex-block">
+        <span class="text-alert">&lt;H4&gt;</span>
+        <span class="pl-1">
+            Intuition, dogma and an inward home.
+        </span>
+        <span class="text-alert">&lt;H4&gt;</span>
+      </h4>
+      <p class="w-50 flex-block">
+        <span class="text-alert">&lt;p&gt;</span>
+        <span class="pl-2">
+          Paradox acquires when you receive with sainthood. Emitters reproduce from tragedies like seismic green people.
+          Love visualizes when you shape with truth. Who can understand the acceptance and afterlife of a wind if he has the
+          great booda-hood of the follower?
+        </span>
+        <span class="text-alert">&lt;p&gt;</span>
       </p>
-      <p class="w-50 text-center">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab dolorum eaque eveniet harum hic id itaque
-        molestiae natus nobis odit, officiis optio quasi quibusdam reiciendis repudiandae sequi tenetur ullam, velit?
-      </p>
+      <code class="w-75 py-2 text-center">
+        All resources for responsive font, you will find inside "_font-mixins" & "_font_respone"(default) files
+      </code>
+    </div>
   </div>
 </template>
 
